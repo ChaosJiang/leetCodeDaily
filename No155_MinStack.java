5b58a45f03d724e7460a273162f27578a581ea4e
[dayDreamer] <[codemelody@163.com]>
2021-02-26 22:26:35 +0900

    No155_MinStack answer 2

--- No155_MinStack.java
+++ No155_MinStack.java
@@ -5,33 +5,35 @@ import java.util.ArrayList;
  * File Name: Bo155_MinStack
  * Created by Chiangsir.
  * Date: AD 2021/02/26
- * description: 常规方法，为了能在常数时间内找到最小值，每次push，pop后都重新查找最小值，并保存至变量minIndex
+ * description: 用空间换取时间，增加额外的minStrack。每次入栈后，就把栈的最小值保存至minStrack
  */
 public class No155_MinStack {
     /** initialize your data structure here. */
     private ArrayList<Integer> stack;
+    private ArrayList<Integer> min;
     private int count;
-    private int minIndex;
 
     public No155_MinStack() {
         this.stack = new ArrayList<Integer>();
-        this.minIndex = 0;
+        this.min = new ArrayList<Integer>();
+        this.count = 0;
     }
 
     public void push(int x) {
         this.stack.add(x);
+        if(this.count == 0) {
+            this.min.add(x);
+        } else {
+            this.min.add(x < this.min.get(count -1) ? x : this.min.get(count -1));
+        }
         ++this.count;
-        if(x < this.stack.get(minIndex)) minIndex = this.count -1;
     }
 
     public void pop() {
         if(this.count == 0) return;
-        this.stack.remove(this.count -1);
+        this.stack.remove(count -1);
+        this.min.remove(count -1);
         --this.count;
-        this.minIndex = 0;
-        for (int i = 0; i < this.count; i++) {
-            if(this.stack.get(i) < this.stack.get(minIndex)) this.minIndex = i;
-        }
     }
 
     public int top() {
@@ -39,6 +41,6 @@ public class No155_MinStack {
     }
 
     public int getMin() {
-        return this.count == 0 ? null: this.stack.get(minIndex);
+        return this.count == 0 ? null: this.min.get(count - 1);
     }
 }
